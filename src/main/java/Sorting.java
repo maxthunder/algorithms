c9e31d881a458bb68bb0d7c53de89abc64cc004b
maxthunder <maxwell.stark@gmail.com>
2019-11-17 15:27:39 -0700

    Adds QuickSort and sorting runner/tester framework

--- src/main/java/Sorting.java
+++ src/main/java/Sorting.java
@@ -1,40 +1,63 @@
-
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Sorting {
 
-    // TODO: Big O info
+    public enum Type {
+        QUICK_SORT("QuickSort");
+        private String name;
+        Type(String name) { this.name = name; }
+        public String getName() { return name; }
+    }
 
     /**
-     * Quick Sort. List must be non-empty.
+     * Quick Sort Implementation. List must be non-empty.
      *
      * PERFORMANCE:
      *  Worst-case: O(n^2)
      *  Average-case: O(n log n)
-     *  Best-case: O(n log n)
-     *
-     * @param inputList List to sort. Must be non-empty
-     * @return Sorted list.
+     *  Best-case: O(n log n)     *
+     * @param inputList List to sort. Must be non-empty.
+     * @return Sorted list
      *
      */
-    public List<Integer> quickSort(List<Integer> inputList) {
+    public static List<Integer> quickSort(List<Integer> inputList) {
+        if (inputList == null || inputList.isEmpty())
+            throw new IllegalStateException("Input list must be non-empty.");
         if (inputList.size() == 1)
             return inputList;
-
-        Integer pivot = inputList.get(0);
-        for (int i = 0; i < inputList.size() - 1; i++) {// skip lastItem/pivot
-            
+        if (inputList.size() == 2) {
+            if (inputList.get(0) > inputList.get(1))
+                Collections.reverse(inputList);
+            return inputList;
         }
 
+        Integer pivot = inputList.get(inputList.size() - 1);
+        List<Integer> lessThanOrEqual = new ArrayList<>();
+        List<Integer> greaterThan = new ArrayList<>();
+        for (int i = 0; i < inputList.size() - 1; i++) {// skip lastItem/pivot
+            Integer item = inputList.get(i);
+            if (item <= pivot)
+                lessThanOrEqual.add(item);
+            else
+                greaterThan.add(item);
+        }
 
-        List<Integer> greaterThanPivot = inputList.stream()
-                .parallel()
-                .filter(i -> i > pivot)
-                .collect(Collectors.toList());
+        List<Integer> completedList = (lessThanOrEqual.size() > 1) ? quickSort(lessThanOrEqual) : lessThanOrEqual;
+        completedList.add(pivot);
+        completedList.addAll((greaterThan.size() > 1) ? quickSort(greaterThan) : greaterThan);
 
-        return null;
+        return completedList;
     }
+
+    // if (1 item)
+    //      - return list (of item)
+    // if (2 items)
+    //      - sort items manually
+    //      - return list
+    // if (3+ item)
+    //      - quick sort
+    //      - return list of item
 
 }
---
+++ src/test/java/SortingTest.java
@@ -0,0 +1,55 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.junit.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SortingTest {
+
+    @Test
+    public void RunAllSortingAlgorithms() {
+        long seed = 17834589345L;
+        int runSize = 200_000;
+        final List<Integer> unsortedList = numberGen(seed, runSize);
+
+        List<Integer> expected = new ArrayList<>(unsortedList);
+        Collections.sort(expected);
+
+        Map<Sorting.Type, SortingResult> algorithmsWithResults = new HashMap<>();
+        algorithmsWithResults.put(Sorting.Type.QUICK_SORT, quickSort(unsortedList));
+
+        algorithmsWithResults.forEach((key, value) -> {
+            System.out.printf("%s sorted %,d integers in %,d ms.%n", key, runSize, value.runtime);
+            assertThat("Quick sorted list did not match expected list.", value.resultList, is(expected));
+        });
+    }
+
+    private SortingResult quickSort(List<Integer> unsortedList) {
+        Instant start = Instant.now();
+        List<Integer> sortedList = Sorting.quickSort(unsortedList);
+        Instant finish = Instant.now();
+        return new SortingResult(sortedList, Duration.between(start, finish).toMillis());
+    }
+
+    private List<Integer> numberGen(long seed, int runSize) {
+        List<Integer> list;
+        Random rand = new Random(seed);
+        list = IntStream.range(0, runSize).mapToObj(i -> rand.nextInt() % 100).collect(Collectors.toList());
+        return list;
+    }
+
+    @Data
+    @AllArgsConstructor
+    private class SortingResult {
+        final List<Integer> resultList;
+        final long runtime;
+    }
+    
+}
