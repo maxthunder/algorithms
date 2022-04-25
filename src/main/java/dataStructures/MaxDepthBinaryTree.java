c1f1d4dd3acb38f7b3622dcd600914c07fae1fcd
maxthunder <maxwell.stark@gmail.com>
2022-04-25 11:28:59 -0600

    Adds doc to max depth binary tree - recursive DFS.

--- src/main/java/dataStructures/MaxDepthBinaryTree.java
+++ src/main/java/dataStructures/MaxDepthBinaryTree.java
@@ -1,6 +1,14 @@
 package dataStructures;
 
 public class MaxDepthBinaryTree {
+
+    /**
+     * Recursive Depth-first Searching
+     * Time complexity: O(n)
+     * Memory storage: O(n), worst case of O(n) can occur if tree is unbalanced.
+     * @param root
+     * @return
+     */
     public static int recursiveDFS(Node root) {
         if (root == null)
             return 0;// should be '-1' if counting edges, whereas right now it's counting nodes
